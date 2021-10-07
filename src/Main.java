a3216a6ebaaa5a2aa17fcd92d51be4ab3fedde39
Eileen Lowers <eileenlowers@outlook.com>
2021-10-07 11:15:07 -0400

    Update gitignore, added collaborator

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
-    //collaborators Zach Snyder
+
+    //collaborators Zach Snyder and Eileen Lowers
+
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
