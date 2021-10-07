8f59813d4ec512966ad4acaecee9f0dd69171977
Zachary Snyder <snyderzw@hotmail.com>
2021-10-07 13:46:18 -0500

    refactoring and commenting

--- src/Ticket.java
+++ src/Ticket.java
@@ -1,17 +1,53 @@
-public class Ticket {
-    private final User user;
-    private final String eta;
-    private final double ticketPrice;
-    private final int boardingPassNumber;
+import java.text.SimpleDateFormat;
 
+public class Ticket {
+    /**
+     * stores information input by the user
+     */
+    private final User user;
+    /**
+     * a timestamp of the estimated arival time
+     */
+    private final String eta;
+    /**
+     * the calculated cost of the ticket for the user
+     */
+    private final double ticketPrice;
+    /**
+     * a unique identifier for the ticket
+     */
+    private final int boardingPassNumber;
+    /**
+     * a number that is used to set boardingPassNumber
+     * this ensures there will never be duplicates
+     */
     private static int currentBoardingPassNumber = 100;
 
-    public Ticket(User user, String eta, double ticketPrice) {
+    /**
+     * The full constructor for the ticket class.
+     *
+     * @param user               the user requesting a ticket
+     * @param eta                the estimated time of arrival
+     * @param ticketPrice        the price the user will pay for the ticket
+     * @param boardingPassNumber the unique identifier for the ticket
+     */
+    private Ticket(User user, String eta, double ticketPrice, int boardingPassNumber) {
         this.user = user;
         this.eta = eta;
-        this.ticketPrice = getTicketPrice();
-        this.boardingPassNumber = getNewBoardingPassNumber();
+        this.ticketPrice = ticketPrice;
+        this.boardingPassNumber = boardingPassNumber;
+    }
 
+    /**
+     * The preferred constructor for the Ticket class
+     * Fills the unspecified fields with the getNew methods
+     * @param user the user requesting a ticket
+     */
+    private Ticket(User user) {
+        this.user = user;
+        eta = getNewEta();
+        ticketPrice = getNewTicketPrice();
+        boardingPassNumber = getNewBoardingPassNumber();
     }
 
     @Override
@@ -24,38 +60,52 @@ public class Ticket {
                 '}';
     }
 
-    public User getUser() {
-        return user;
+    /**
+     * generates a new eta
+     * @return a new eta
+     */
+    private String getNewEta() {
+        SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return f.format(System.currentTimeMillis());
     }
 
-    public String getEta() {
-        String departure = user.getDepartureTime();
-
-        return eta;
-    }
-
-    public double getTicketPrice() {
+    /**
+     * calculates the price for a new ticket
+     * @return the price for a new ticket
+     */
+    public double getNewTicketPrice() {
 
         double price = 10d;
         int age = user.getAge();
 
-        if(age <= 12){ price*=.5;}
-        if(age >= 60){ price*=.4;}
-        if(user.getGender().equals("f")){ price*=.75;}
+        if (age <= 12) {
+            price *= .5;
+        }
+        if (age >= 60) {
+            price *= .4;
+        }
+        if (user.getGender().equals("f")) {
+            price *= .75;
+        }
 
         return price;
     }
 
-    public int getBoardingPassNumber() {
-        return boardingPassNumber;
-    }
-
-    public static int getCurrentBoardingPassNumber() {
-        return currentBoardingPassNumber;
-    }
-
+    /**
+     * increments the previous boardingPassNumber and returns the value
+     * makes sure that no two boardingPassNumbers are the same
+     * @return a new boardingPassNumber valued 1 more than the last
+     */
     public int getNewBoardingPassNumber() {
         currentBoardingPassNumber++;
         return currentBoardingPassNumber;
     }
+
+    public User getUser() {return user;}
+
+    public String getEta() {return eta;}
+
+    public int getBoardingPassNumber() {return boardingPassNumber;}
+
+    public static int getCurrentBoardingPassNumber() {return currentBoardingPassNumber;}
 }
--- src/TicketProcessor.java
+++ src/TicketProcessor.java
@@ -2,9 +2,19 @@ import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * This class is for reading and writing tickets and stubs to files
+ */
 public class TicketProcessor {
+    /**
+     * This holds a map of the tickets in memory.
+     * This map is used as an object that gets serialized to and from files.
+     */
     private static Map<Integer, Ticket> tickets = new  HashMap<>();
 
+    /**
+     * This method serializes the tickets map to a file.
+     */
     public static void writeTickets(){
         try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("src\\com\\company\\tickets.txt"))){
             oos.writeObject(tickets);
@@ -13,6 +23,9 @@ public class TicketProcessor {
         }
     }
 
+    /**
+     * This method reads the serialized map from a file and stores it in the tickets map instance variable.
+     */
     @SuppressWarnings("unchecked")
     public static void readTickets() {
         try{
@@ -27,6 +40,10 @@ public class TicketProcessor {
         }
     }
 
+    /**
+     * this method is used to add tickets to the map and store them in files.
+     * @param t is the ticket to be stored
+     */
     public static void addTicket(Ticket t){
         tickets.put(t.getBoardingPassNumber(), t);
         writeTickets();
