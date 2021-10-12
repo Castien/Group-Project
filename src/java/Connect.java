ce75e8e4f9fda7be7e337f772ca6b2d7507b4fc7
Eileen Lowers <eileenlowers@outlook.com>
2021-10-12 15:18:09 -0400

    replacing establishSave

--- src/java/Connect.java
+++ src/java/Connect.java
@@ -70,6 +70,50 @@ public class Connect {
         }
     }
 
+    /**
+     * Insert into the ticket table
+     * @param u - User object containing the data to be inserted
+     * @param t - Ticket object containing the data to be inserted
+     */
+    public static Connection establishSave(User u, Ticket t) {
+
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/boarding_pass", "root", "root");
+            if (conn != null) {
+                String temp = "INSERT INTO ticket (boardingPassNumber, " +
+                        "name, email, phoneNumber, gender, " +
+                        "age, destination, departureTime, eta, ticketPrice) " +
+                        "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                PreparedStatement prep = conn.prepareStatement(temp);
+                {
+                    prep.setString(1, String.valueOf(t.getBoardingPassNumber()));
+                    prep.setString(2, u.getName());
+                    prep.setString(3, u.getEmail());
+                    prep.setString(4, u.getPhoneNumber());
+                    prep.setString(5, u.getGender());
+                    prep.setString(6, String.valueOf(u.getAge()));
+                    prep.setString(7, u.getDestination());
+                    prep.setString(8, u.getDepartureTime());
+                    prep.setString(9, t.getEta());
+                    prep.setString(10, String.valueOf(t.getTicketPrice()));
+                }
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                if (conn != null && !conn.isClosed()) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return conn;
+    }
+
     public static Map<String, Double> getRoutes() {
         return routes;
     }
