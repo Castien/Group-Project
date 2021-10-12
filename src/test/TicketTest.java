28e46b07b7ed9ccb21a5dcdc34b1f755cbe949af
Eileen Lowers <eileenlowers@outlook.com>
2021-10-12 15:10:18 -0400

    JUnit test for Ticket

--- src/test/TicketTest.java
+++ src/test/TicketTest.java
@@ -5,31 +5,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TicketTest {
 
+    private User user;
+    private Ticket ticket;
+
     @BeforeEach
     void setUp() {
-    }
-
-    @Test
-    void testToString() {
-    }
-
-    @Test
-    void getUser() {
+        user = new User("Bob", "bob@email.com",
+                "555-555-5555", "m", 25,
+                "Tokyo", "1:00" );
+        ticket = new Ticket("2:00", 10.00, 101, user);
     }
 
     @Test
     void getEta() {
+        System.out.println(ticket.getEta());
+        assertEquals("2:00", ticket.getEta(), "getEta Failed");
     }
 
     @Test
     void getTicketPrice() {
+        System.out.println(ticket.getTicketPrice());
+        assertEquals(10.00, ticket.getTicketPrice(), "getTicketPrice Failed");
     }
 
     @Test
     void getBoardingPassNumber() {
-    }
-
-    @Test
-    void getCurrentBoardingPassNumber() {
+        System.out.println(ticket.getBoardingPassNumber());
+        assertEquals(101, ticket.getBoardingPassNumber(), "getBoardingPassNumber Failed");
     }
 }
