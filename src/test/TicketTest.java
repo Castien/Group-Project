f5225b765ae26794195f5f6f2321ffa6813220f3
Eileen Lowers <eileenlowers@outlook.com>
2021-10-12 14:04:11 -0400

    generating JUnit test files

---
+++ src/test/TicketTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TicketTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void getUser() {
+    }
+
+    @Test
+    void getEta() {
+    }
+
+    @Test
+    void getTicketPrice() {
+    }
+
+    @Test
+    void getBoardingPassNumber() {
+    }
+
+    @Test
+    void getCurrentBoardingPassNumber() {
+    }
+}
---
+++ src/test/UserTest.java
@@ -0,0 +1,71 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+
+    @Test
+    void getPhoneNumber() {
+    }
+
+    @Test
+    void setPhoneNumber() {
+    }
+
+    @Test
+    void getGender() {
+    }
+
+    @Test
+    void setGender() {
+    }
+
+    @Test
+    void getAge() {
+    }
+
+    @Test
+    void setAge() {
+    }
+
+    @Test
+    void getDestination() {
+    }
+
+    @Test
+    void setDestination() {
+    }
+
+    @Test
+    void getDepartureTime() {
+    }
+
+    @Test
+    void setDepartureTime() {
+    }
+}
