699517a55b84eb0750c8f490d4146f4f38bef320
Zachary Snyder <snyderzw@hotmail.com>
2021-10-12 14:03:24 -0500

    refactoring changes

--- src/java/Connect.java
+++ src/java/Connect.java
@@ -1,10 +1,23 @@
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+/**
+ * this class handles the database that is connected through a local mysql server.
+ * It retrieves route information and stores ticket information to and from this database.
+ */
 public class Connect {
 
-    private static ArrayList<Route> routes;
+    /**
+     * a list that holds all the connections that the user can go to and the time they take to reach
+     */
+    private static Map<String, Double> routes = new HashMap<>();
 
+    /**
+     * establishes a connection to the database on a local mysql server
+     */
     public static void establishConnection(){
 
         Connection conn = null;
@@ -12,28 +25,7 @@ public class Connect {
             conn = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/boarding_pass","root","root");
             if (conn != null) {
-                Statement statement = conn.createStatement();
-                ResultSet results = statement.executeQuery("SELECT destination FROM boarding_pass.route");
-
-                ArrayList<String> destinations = new ArrayList<>();
-                while(results.next()){
-                    destinations.add(results.getString(1));
-                }
-
-                results = statement.executeQuery("SELECT trip_time FROM boarding_pass.route");
-
-                ArrayList<String> times = new ArrayList<>();
-                while(results.next()){
-                    times.add(results.getString(1));
-                }
-
-                routes = new ArrayList<>();
-                if(destinations.size() == times.size()){
-                    for (int i=0; i< destinations.size(); i++){
-                        routes.add(new Route(destinations.get(i), Double.parseDouble(times.get(i))));
-                    }
-                }
-                routes.forEach(System.out::println);
+                collectInfo(conn);
             }
 
         } catch (SQLException ex) {
@@ -49,7 +41,39 @@ public class Connect {
         }
     }
 
-    public static ArrayList<Route> getRoutes() {
+    /**
+     * gathers information stored in the database and places it into the route instance variable
+     * @param conn the connection to the database
+     * @throws SQLException for sql queries
+     */
+    private static void collectInfo(Connection conn) throws SQLException {
+        Statement statement = conn.createStatement();
+        ResultSet results = statement.executeQuery("SELECT destination FROM boarding_pass.route");
+
+        ArrayList<String> destinations = new ArrayList<>();
+        while(results.next()){
+            destinations.add(results.getString(1));
+        }
+
+        results = statement.executeQuery("SELECT trip_time FROM boarding_pass.route");
+
+        ArrayList<String> times = new ArrayList<>();
+        while(results.next()){
+            times.add(results.getString(1));
+        }
+
+        if(destinations.size() == times.size()){
+            for (int i=0; i< destinations.size(); i++){
+                routes.put(destinations.get(i), Double.parseDouble(times.get(i)));
+            }
+        }
+    }
+
+    public static Map<String, Double> getRoutes() {
         return routes;
+    }
+
+    public static void setRoutes(Map<String, Double> routes) {
+        Connect.routes = routes;
     }
 }
--- src/java/Display.java
+++ src/java/Display.java
@@ -3,11 +3,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Locale;
 
 import static java.lang.String.format;
 
 public class Display {
 
+    /**
+     * frame to display components with
+     */
     private static JFrame f;
 
     /**
@@ -39,15 +43,35 @@ public class Display {
      */
     private static JRadioButton genderOtherButton;
 
+    /**
+     * spinner for the age of the user. Cannot be blank or contain a letter
+     */
     private static JSpinner ageSpinner;
 
+    /**
+     * combo box that contains the destinations pulled in from the database.
+     */
     private static JComboBox<String> destinationField;
+    /**
+     * combo box that contains information about the time required to reach a certain destination.
+     * Also pulled from the database.
+     */
     private static JComboBox<String> departureTimeField;
 
+    /**
+     * error label starts blank and gets overwritten whenever an error occurs to tell the user what they did wrong.
+     */
     private static JLabel errorLabel;
 
+    /**
+     * regex expression that matches any valid phone number
+     */
     private static final String REGEX_FOR_PHONE = "^(\\+\\d{1,2}\\s)?\\(?\\d{3}\\)?[\\s.-]?\\d{3}[\\s.-]?\\d{4}$";
 
+    /**
+     * regular expression that matches any valid email.
+     * Doesn't like capital letters so strings must be toLower.
+     */
     private static final String REGEX_FOR_EMAIL = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)" +
             "*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x" +
             "7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0" +
@@ -55,7 +79,11 @@ public class Display {
             "-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f" +
             "])+)\\])";
 
-
+    /**
+     * this method launches the first gui window that allows the user to fill out a form.
+     * That form is used to create a ticket for the user.
+     * Continues on to the launchSecondGui method.
+     */
     public static void launchGui() {
 
         if (f != null) f.dispose();
@@ -65,8 +93,12 @@ public class Display {
         f.setLayout(null);
 
         String[] destinations = new String[Connect.getRoutes().size()];
-        for(int i=0; i<Connect.getRoutes().size(); i++){
-            destinations[i] = Connect.getRoutes().get(i).getDestination();
+        {
+            int i = 0;
+            for (String d : Connect.getRoutes().keySet()) {
+                destinations[i] = d;
+                i++;
+            }
         }
 
         final int NUM_TIMES = 24;
@@ -88,68 +120,68 @@ public class Display {
 
         errorLabel = new JLabel("", JLabel.RIGHT);
 
-        int border = 20;
-        int gap = 10;
-
-        int width = 125;
-
-        int x2 = width + border + gap;
-
-        int spacing = border + gap;
+        final int border = 20;
+        final int gap = 10;
+        final int width1 = 125, width2 = 200;
+        final int x2 = width1 + border + gap;
+        final int spacing = border + gap;
 
         String[] labelText = {"Name: ", "Email: ", "Phone Number: ",
                 "Gender: ", "Age: ", "Destination: ", "Departure Time: "};
 
         for (int i = 0; i < labelText.length; i++) {
-            addLabelToFrame(labelText[i], spacing, border, border, width, border, i);
+            addLabelToFrame(labelText[i], spacing, border, border, width1, border, i);
         }
 
-        width += 75;
-
-        nameField.setBounds(x2, border, width, border);
+        nameField.setBounds(x2, border, width2, border);
         f.add(nameField);
 
-        emailField.setBounds(x2, border + spacing, width, border);
+        emailField.setBounds(x2, border + spacing, width2, border);
         f.add(emailField);
 
-        phoneNumberField.setBounds(x2, border + (2 * spacing), width, border);
+        phoneNumberField.setBounds(x2, border + (2 * spacing), width2, border);
         f.add(phoneNumberField);
 
         ButtonGroup bg = new ButtonGroup();
 
-        genderMaleButton.setBounds(x2, border + (3 * spacing), width/4, border);
+        genderMaleButton.setBounds(x2, border + (3 * spacing), width2/4, border);
         bg.add(genderMaleButton);
         f.add(genderMaleButton);
 
-        genderFemaleButton.setBounds(x2 + 50, border + (3 * spacing), width/4, border);
+        genderFemaleButton.setBounds(x2 + 50, border + (3 * spacing), width2/4, border);
         bg.add(genderFemaleButton);
         f.add(genderFemaleButton);
 
-        genderOtherButton.setBounds(x2 + 100, border + (3 * spacing), width/4, border);
+        genderOtherButton.setBounds(x2 + 100, border + (3 * spacing), width2/4, border);
         bg.add(genderOtherButton);
         f.add(genderOtherButton);
 
-        ageSpinner.setBounds(x2, border + (4 * spacing), width/4, border);
+        ageSpinner.setBounds(x2, border + (4 * spacing), width2/4, border);
         f.add(ageSpinner);
 
-        destinationField.setBounds(x2, border + (5 * spacing), width, border);
+        destinationField.setBounds(x2, border + (5 * spacing), width2, border);
         f.add(destinationField);
 
-        departureTimeField.setBounds(x2, border + (6 * spacing), width, border);
+        departureTimeField.setBounds(x2, border + (6 * spacing), width2, border);
         f.add(departureTimeField);
 
         JButton b = new JButton("submit");
-        b.setBounds(x2, border + (labelText.length * spacing), width, (border * 2));
+        b.setBounds(x2, border + (labelText.length * spacing), width2, (border * 2));
         b.addActionListener(new sentButtonClickedActionListener());
         f.add(b);
 
-        errorLabel.setBounds(x2, border + ((labelText.length + 2) * spacing), width, border);
+        errorLabel.setBounds(x2, border + ((labelText.length + 2) * spacing), width2, border);
         f.add(errorLabel);
 
-        f.setSize((width * 2), ((border + spacing) * labelText.length));
+        f.setSize((width2 * 2), ((border + spacing) * labelText.length));
         f.setVisible(true);
     }
 
+    /**
+     * gui window that displays the ticket information to the user.
+     * returns to the original window so another user can fill out a new form once the ok button is clicked.
+     * @param t the ticket to display information for
+     */
     public static void launchSecondGui(Ticket t) {
 
         User u = t.getUser();
@@ -187,6 +219,17 @@ public class Display {
         f.setVisible(true);
     }
 
+    /**
+     * is used to arrange JLabel components in a vertical column.
+     * each component is placed y + (index * spacing) units below the first
+     * @param labelString the string for the JLabel
+     * @param spacing how far apart each component should be
+     * @param x the horizontal alignment of the components
+     * @param y the y value of the first component at index 0
+     * @param width how wide the component should be
+     * @param height how tall the component should be
+     * @param index how many components worth of space should be given
+     */
     private static void addLabelToFrame(String labelString,
                                         int spacing, int x, int y, int width, int height, int index) {
 
@@ -195,6 +238,18 @@ public class Display {
         f.add(label);
     }
 
+    /**
+     * is used to arrange JLabel components in two vertical columns.
+     * labelString is for the first column and infoString is next to them on the second column
+     * each component is placed y + (index * spacing) units below the first
+     * @param labelString the string for the JLabel
+     * @param spacing how far apart each component should be
+     * @param x the horizontal alignment of the components
+     * @param y the y value of the first component at index 0
+     * @param width how wide the component should be
+     * @param height how tall the component should be
+     * @param index how many components worth of space should be given
+     */
     private static void addLabelAndInfoToFrame(String labelString, String infoString,
                                                int spacing, int x, int y, int width, int height, int index) {
 
@@ -207,6 +262,11 @@ public class Display {
         f.add(info);
     }
 
+    /**
+     * utility method that makes sure a nullPointer exception is not created
+     * @param age a string representing an age
+     * @return age parsed into an integer, 0 when blank
+     */
     private static int parseAge(String age) {
         if (!age.equals("")) {
             return Integer.parseInt(age);
@@ -214,10 +274,14 @@ public class Display {
         return 0;
     }
 
+    /**
+     * the actionListener that collects all the information from the forms and makes sure they are valid.
+     * if valid it creates a ticket using that information, writes it to a stub, and sends the user to the second gui.
+     */
     private static class sentButtonClickedActionListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             String name = nameField.getText();
-            String email = emailField.getText();
+            String email = emailField.getText().toLowerCase();
             String phoneNumber = phoneNumberField.getText();
 
             String gender;
@@ -250,6 +314,9 @@ public class Display {
         }
     }
 
+    /**
+     * the action click listener for the ok button that returns the user back to the first gui window.
+     */
     private static class okButtonClickedActionListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             launchGui();
